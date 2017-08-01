66640f32c119a35b5c40e92a2595f5241c474b00
Peter J. Kohler, PhD <pjkohl3r@gmail.com>
2017-08-01 09:29:20 -0700

    unwarp wrapper (uses gru code)

---
+++ matlab/mriUnwarp.m
@@ -0,0 +1,30 @@
+function mriUnwarp(dataDir)
+    addpath(genpath('/Users/kohler/Dropbox/CODE/git/gardner/gru'));
+    if ~iscell(dataDir)
+        tempDir = {dataDir}; % wrap it
+        clear dataDir;
+        dataDir = tempDir;
+    else
+    end
+    for d = 1:length(dataDir)
+        if exist(dataDir{d},'dir')
+            unwarp.EPIfiles = subfiles([dataDir{d},'/run*nii*']);
+            unwarp.calfiles = subfiles([dataDir{d},'/*unwarp*nii*']);
+            unwarp.EPIfiles = unwarp.EPIfiles(~ismember(unwarp.EPIfiles,unwarp.calfiles));
+            % get rid of anatomy files
+            anat = [subfiles([dataDir{d},'/*T1*']);...
+                    subfiles([dataDir{d},'/*T2*']);...
+                    subfiles([dataDir{d},'/*inplane*'])];
+            for a = 1:length(anat)
+                if ischar(anat{a})
+                    unwarp.EPIfiles = unwarp.EPIfiles(~ismember(unwarp.EPIfiles,anat{a}));
+                else
+                end
+            end            
+            fsl_pe0pe1(dataDir{d},unwarp);
+        else
+            warning([dataDir{d}, ' does not exist']);
+        end
+        clear unwarp;
+    end
+end
