67e21c26d3739a4c05ee640a16f4d749b21fb8e2
Frank Thomas <fhethomas@gmail.com>
2018-06-07 15:01:23 -0700

    Amended init function

--- python/mymri/__init__.py
+++ python/mymri/__init__.py
@@ -1,8 +1,8 @@
 try:
-	from main import Suma, Neuro2Radio, Pre, Volreg, Scale, Vol2Surf, Surf2Vol, RoiTemplates, RoiSurfData, MriSurfSmooth, MriFFT, HotT2Test, outdata_loader
+	from main import Suma, Neuro2Radio, Pre, Volreg, Scale, Vol2Surf, Surf2Vol, RoiTemplates, RoiSurfData, MriSurfSmooth, MriFFT, HotT2Test
 	from utils import BidsOrganizer, BidsLinks
 except ImportError:
-	from .main import Suma, Neuro2Radio, Pre, Volreg, Scale, Vol2Surf, Surf2Vol, RoiTemplates, RoiSurfData, MriSurfSmooth, MriFFT, HotT2Test, outdata_loader
+	from .main import Suma, Neuro2Radio, Pre, Volreg, Scale, Vol2Surf, Surf2Vol, RoiTemplates, RoiSurfData, MriSurfSmooth, MriFFT, HotT2Test
 	from .utils import BidsOrganizer, BidsLinks
 
 
