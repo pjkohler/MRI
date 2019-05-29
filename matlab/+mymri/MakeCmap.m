2c2256c7a5c2dbb3e2594231948981a9ac48351d
Peter J. Kohler, PhD <pjkohl3r@gmail.com>
2019-05-29 11:56:59 -0700

    improved colormap creation

--- matlab/+mymri/MakeCmap.m
+++ matlab/+mymri/MakeCmap.m
@@ -1,30 +1,35 @@
-function cMap = mriMakeCmap(numColors,outName,noBlack,noSimple,noShuffle)    
-    if ~exist('distinguishable_colors','file')
-        codeFolder = '/Users/kohler/code';
-        if exist([codeFolder,'/matlab/others/plotTools'],'dir')
-            addpath(genpath([codeFolder,'/matlab/others/plotTools']));
+function cmap = MakeCmap(in_colors,out_name,noBlack,noSimple,noShuffle)
+    if numel(in_colors) == 1
+        if ~exist('distinguishable_colors','file')
+            codeFolder = '/Users/kohler/code';
+            if exist([codeFolder,'/matlab/others/plotTools'],'dir')
+                addpath(genpath([codeFolder,'/matlab/others/plotTools']));
+            else
+                error('distinguishable_colors.m not on path, please add');
+            end
         else
-            error('distinguishable_colors.m not on path, please add');
+        end
+        if noBlack
+            bgColor = [1 1 1; 0 0 0; .5 .5 .5];
+        else
+            bgColor = [1 1 1];
+        end
+        if noSimple
+            bgColor = [bgColor; eye(3); [1 1 0] ];
+        else
+        end
+        cmap = distinguishable_colors(in_colors,bgColor);
+        if ~noShuffle
+            oddIdx = logical(mod(1:in_colors,2));
+            cmap = [cmap(~oddIdx,:); cmap(oddIdx,:)];
+            %cMap = cMap(randperm(in_colors),:);
+        else
         end
     else
+        cmap = in_colors;
     end
-    if noBlack
-        bgColor = [1 1 1; 0 0 0; .5 .5 .5];
-    else
-        bgColor = [1 1 1];
-    end
-    if noSimple
-        bgColor = [bgColor; eye(3); [1 1 0] ];
-    else
-    end
-    cMap = distinguishable_colors(numColors,bgColor);
-    if ~noShuffle
-        oddIdx = logical(mod(1:numColors,2));
-        cMap = [cMap(~oddIdx,:); cMap(oddIdx,:)];
-        %cMap = cMap(randperm(numColors),:);
-    else
-    end
-    cMap = [[1,1,1];cMap];
-    cMap(:,4) = 0:numColors;
-    dlmwrite(outName,cMap,'delimiter','\t','precision',3)
+    cmap = [[1,1,1];cmap];
+    cmap(:,4) = 0:(length(cmap)-1);
+    dlmwrite(out_name,cmap,'delimiter','\t','precision',3)
+    save([out_name,'.mat'],"cmap")
 end
